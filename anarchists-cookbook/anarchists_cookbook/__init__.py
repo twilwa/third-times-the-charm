2acbb4f99a3893ce8c3d82ab68fe823cb14099ef
yikes <spywarezzz@proton.me>
2024-04-03 21:53:35 -0700

    Adjusted imports and references for reddit resources and assets.

--- anarchists-cookbook/anarchists_cookbook/__init__.py
+++ anarchists-cookbook/anarchists_cookbook/__init__.py
@@ -1,15 +1,16 @@
 from dagster import Definitions, load_assets_from_modules
-from .resources.reddit_resource import reddit_client_resource  # Adjusted import
 
 # Assuming you have other assets defined in a module named `assets`
 # and potentially other resources defined elsewhere
 from .assets import reddit_posts_comments_df  # or however you've structured your assets
+from .resources import reddit_resource  # Adjusted import
 
 defs = Definitions(
-    assets=load_assets_from_modules('reddit_posts_comments_df.assets'),  # Adjust as necessary
+    assets=load_assets_from_modules(reddit_posts_comments_df),  # Adjust as necessary
     resources={
-        "reddit_client": reddit_client_resource,  # Adjusted to reference the resource correctly
+        "reddit_client": reddit_resource.reddit_client()
         # Include other resources as before
     },
 )
-#TODO: import redditdatagenerator from .resources, see tutorial/__init__.py
+# TODO: import redditdatagenerator from .resources, see tutorial/__init__.py
+

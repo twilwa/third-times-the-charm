4236d63e19e7a5b3644dbb6335f0d14a79c71fad
yikes <spywarezzz@proton.me>
2024-04-05 10:45:21 -0700

    Updated asset loading to use all assets from the `assets` module and added new imports for asset selection, job definition, schedule definition, and environment variables.

--- anarchists-cookbook/anarchists_cookbook/__init__.py
+++ anarchists-cookbook/anarchists_cookbook/__init__.py
@@ -1,15 +1,23 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import (
+    Definitions,
+    load_assets_from_modules,
+    AssetSelection,
+    define_asset_job,
+    ScheduleDefinition,
+    EnvVar,
+)
+
+from . import assets
 
 # Assuming you have other assets defined in a module named `assets`
-from .assets.reddit_posts_comments_df import reddit_submissions
+
 from .resources.reddit_resource import reddit_client
 
 defs = Definitions(
-    assets=load_assets_from_modules([reddit_submissions]),  # Adjust as necessary
+    assets=load_assets_from_modules([assets]),  # Adjust as necessary
     resources={
         "reddit_client": reddit_client
         # Include other resources as before
     },
 )
 # TODO: import redditdatagenerator from .resources, see tutorial/__init__.py
-

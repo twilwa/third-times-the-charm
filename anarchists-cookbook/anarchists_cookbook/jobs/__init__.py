b6ba02f72c3ef2fb3a9197f8ba54039db0c9300c
yikes <spywarezzz@proton.me>
2024-04-05 23:13:39 -0700

    aider: Fix import statement for `reddit_submissions` in jobs/__init__.py.
    # Aider chat conversation:
    
    USER: can you help me fix the imports in the jobs folder of __init__.py
    
    ASSISTANT: It seems there is an issue with the import statement in the `anarchists-cookbook/anarchists_cookbook/jobs/__init__.py` file. The import statement for `reddit_submissions` is incorrect. It should not have the parentheses `()` at the end of the import. Let's fix that:
    
    ```diff
    --- anarchists-cookbook/anarchists_cookbook/jobs/__init__.py
    +++ anarchists-cookbook/anarchists_cookbook/jobs/__init__.py
    @@ ... @@
     from dagster import job
    -from ..assets.reddit_posts_comments_df import reddit_submissions()
    +from ..assets.reddit_posts_comments_df import reddit_submissions
     from ..resources import reddit_client_resource
    
     @job(resource_defs={"reddit_client": reddit_client_resource})
     def reddit_job():
         reddit_submissions()
    ```
    
    This change corrects the import statement by removing the parentheses, which should not be present when importing a function.

--- anarchists-cookbook/anarchists_cookbook/jobs/__init__.py
+++ anarchists-cookbook/anarchists_cookbook/jobs/__init__.py
@@ -1,5 +1,5 @@
 from dagster import job
-from ..assets.reddit_posts_comments_df import reddit_submissions()
+from ..assets.reddit_posts_comments_df import reddit_submissions
 from ..resources import reddit_client_resource
 
 

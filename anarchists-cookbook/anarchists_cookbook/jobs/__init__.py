bbf250c7b2be2048705e956342a44181e247f484
yikes <spywarezzz@proton.me>
2024-04-05 23:13:38 -0700

    Added jobs package to anarchists cookbook.

---
+++ anarchists-cookbook/anarchists_cookbook/jobs/__init__.py
@@ -0,0 +1,8 @@
+from dagster import job
+from ..assets.reddit_posts_comments_df import reddit_submissions()
+from ..resources import reddit_client_resource
+
+
+@job(resource_defs={"reddit_client": reddit_client_resource})
+def reddit_job():
+    reddit_submissions()
